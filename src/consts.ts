ce7a3f67e86920e61c3ee201f7e76599f0fc4435
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-05-27 21:31:42 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,13 @@
 import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  title: 'Cojocaru David',
+  title: 'Tổ Sư Thiền',
   description:
-    "I'm a Junior Full Stack Developer with a passion for creating web applications. I have experience in both front-end and back-end development, and I'm always eager to learn new technologies and improve my skills. I enjoy collaborating with teams and contributing to projects that make a difference.",
+    "",
   href: 'https://cojocarudavid.me',
-  author: 'Cojocaru David',
+  author: 'Patriarchal Zen',
   locale: 'en-US',
-  location: 'Romania',
+  location: 'India',
 }
 
 export const NAV_LINKS: SocialLink[] = [
